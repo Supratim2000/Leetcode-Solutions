9bebeed51f22af167b22587a1acc4ea47167eac6
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2020-10-05 19:53:04 +0530

    Create Sqrt(x).cpp

---
+++ Sqrt(x).cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0)
+            return 0;
+        if(x==1)
+            return 1;
+        long long low = 0;
+        long long high = x;
+        long long mid;
+        while(low<=high)
+        {
+            mid=low+(high-low)/2;
+            if(mid*mid == x)
+                return mid;
+            else if(mid*mid > x)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return low-1;
+    }
+};
