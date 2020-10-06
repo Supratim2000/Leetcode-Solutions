ad75e3dcf32c7b8f3b0dc890d4750a2522705eb5
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2020-10-06 10:06:33 +0530

    Update Sqrt(x).cpp

--- Sqrt(x).cpp
+++ Sqrt(x).cpp
@@ -3,19 +3,19 @@ public:
     int mySqrt(int x) {
         if(x==0)
             return 0;
-        long long low = 0;
-        long long high = x;
+        int low = 0;
+        int high = x;
         long long mid;
-        while(low<=high)
-        {
+        int ans;
+        while(low<=high) {
             mid=low+(high-low)/2;
-            if(mid*mid == x)
-                return mid;
-            else if(mid*mid > x)
-                high = mid - 1;
-            else
+            if(mid*mid <= x) {
+                ans=mid;
                 low = mid + 1;
+            }
+            else
+                high = mid - 1;
         }
-        return low-1;
+        return ans;
     }
 };
