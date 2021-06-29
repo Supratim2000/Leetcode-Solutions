24ee97694a6919ccd9ced2eb3bb7c9e7adbfd83a
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-06-29 11:36:50 +0530

    Update Range Sum of BST.cpp

--- Range Sum of BST.cpp
+++ Range Sum of BST.cpp
@@ -3,7 +3,11 @@ public:
     int rangeSumBST(TreeNode* root, int low, int high) {
         if(root==nullptr)
             return 0;
-        int subtreeSum=rangeSumBST(root->left,low,high)+rangeSumBST(root->right,low,high);
-        return root->val>=low && root->val<=high?subtreeSum+root->val:subtreeSum;
+        if(root->val>=low && root->val<=high)
+            return root->val+rangeSumBST(root->left,low,high)+rangeSumBST(root->right,low,high);
+        else if(root->val<low)
+            return rangeSumBST(root->right,low,high);
+        else
+            return rangeSumBST(root->left,low,high);
     }
 };
