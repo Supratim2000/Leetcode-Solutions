1a416e44c7777fc502c29299687329731a141321
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-06-29 10:35:46 +0530

    Update Search in a Binary Search Tree.cpp

--- Search in a Binary Search Tree.cpp
+++ Search in a Binary Search Tree.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //Recursive
     TreeNode* searchBST(TreeNode* root, int val) {
         if(root==nullptr)
             return nullptr;
@@ -8,5 +9,22 @@ public:
         else if(root->val<val)
             return searchBST(root->right,val);
         return searchBST(root->left,val);
+    }
+    
+    //Iterative
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root==nullptr)
+            return nullptr;
+        TreeNode* p=root;
+        while(p)
+        {
+            if(p->val==val)
+                return p;
+            else if(p->val<val)
+                p=p->right;
+            else
+                p=p->left;
+        }
+        return nullptr;
     }
 };
