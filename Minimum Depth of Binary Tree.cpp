de1a7d264a3b9baf6ac34c82151edacb8a3b3265
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-07-01 11:06:09 +0530

    Update Minimum Depth of Binary Tree.cpp

--- Minimum Depth of Binary Tree.cpp
+++ Minimum Depth of Binary Tree.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //Method 1
     int minDepth(TreeNode* root) {
         if(root==nullptr)
             return 0;
@@ -10,5 +11,31 @@ public:
         if(root->right==nullptr)
             return minDepth(root->left)+1;
         return min(minDepth(root->right),minDepth(root->left))+1;
+    }
+        
+    //Method 2
+    int minDepth(TreeNode* root) {
+        if(root==nullptr)
+            return 0;
+        queue<TreeNode*> pendingNodes;
+        pendingNodes.push(root);
+        int level=1;
+        while(!pendingNodes.empty())
+        {
+            int n=pendingNodes.size();
+            for(int i=0;i<n;i++)
+            {
+                TreeNode* cur=pendingNodes.front();
+                pendingNodes.pop();
+                if(cur->left==nullptr && cur->right==nullptr)
+                    return level;
+                if(cur->left!=nullptr)
+                    pendingNodes.push(cur->left);
+                if(cur->right!=nullptr)
+                    pendingNodes.push(cur->right);
+            }
+            level++;
+        }
+        return level;
     }
 };
