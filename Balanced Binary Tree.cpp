05376063340866559b980d693f010bdbc97df48f
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-07-01 14:52:11 +0530

    Update Balanced Binary Tree.cpp

--- Balanced Binary Tree.cpp
+++ Balanced Binary Tree.cpp
@@ -39,5 +39,66 @@ public:
         height(root,balanced);
         return balanced;
     }
+};
+
+//Optimised O(n) with a function which returns both height and isHieghtBalanced simultaniously in pair
+class Solution {
+public:    
+    pair<int,bool> treeBalanced(TreeNode* root)
+    {
+        if(root==nullptr)
+        {
+            pair<int,bool> ans;
+            ans.first=0;
+            ans.second=true;
+            return ans;
+        }
+        else if(root->left==nullptr && root->right==nullptr)
+        {
+            pair<int,bool> ans;
+            ans.first=1;
+            ans.second=true;
+            return ans;
+        }
+        else if(root->left==nullptr && root->right!=nullptr)
+        {
+            pair<int,bool> ans;
+            pair<int,bool> right=treeBalanced(root->right);
+            
+            if(right.second && right.first==1)
+                ans.second=true;
+            else
+                ans.second=false;
+            ans.first=right.first+1;
+            return ans;
+        }
+        else if(root->left!=nullptr && root->right==nullptr)
+        {   
+            pair<int,bool> ans;
+            pair<int,bool> left=treeBalanced(root->left);
+            if(left.second && left.first==1)
+                ans.second=true;
+            else
+                ans.second=false;
+            ans.first=left.first+1;
+            return ans;
+        }
+        else
+        {
+            pair<int,bool> ans;
+            pair<int,bool> left=treeBalanced(root->left);
+            pair<int,bool> right=treeBalanced(root->right);
+            if(left.second && right.second && abs(left.first-right.first)<=1)
+                ans.second=true;
+            else
+                ans.second=false;
+            ans.first=max(left.first,right.first)+1;
+            return ans;
+        }
+    }
     
+    bool isBalanced(TreeNode* root) {
+        pair<int,bool> ans=treeBalanced(root);
+        return ans.second;
+    }  
 };
