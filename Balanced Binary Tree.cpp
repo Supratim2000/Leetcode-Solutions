8697d910c9db800b6539de32a812c1a8913221b8
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-07-01 12:40:51 +0530

    Update Balanced Binary Tree.cpp

--- Balanced Binary Tree.cpp
+++ Balanced Binary Tree.cpp
@@ -1,3 +1,4 @@
+//Unoptimised O(n^2)
 class Solution {
 public:
     
@@ -5,7 +6,6 @@ public:
     {
         return !root?0:max(height(root->left),height(root->right))+1;
     }
-    
     bool isBalanced(TreeNode* root) {
         if(root==nullptr)
             return true;
@@ -15,4 +15,29 @@ public:
         if(!right) return false;
         return left && right && abs(height(root->left)-height(root->right))<=1;
     }
+};
+
+//Optimised O(n)
+class Solution {
+public:    
+    int height(TreeNode* root,bool &balanced)
+    {
+        if(root==nullptr)
+            return 0;
+        int leftHeight=height(root->left,balanced);
+        int rightHeight=height(root->right,balanced);
+        if(abs(leftHeight-rightHeight)>1)
+        {
+            balanced=false;
+            return 10000;
+        }
+        return max(leftHeight,rightHeight)+1;
+    }
+
+    bool isBalanced(TreeNode* root) {
+        bool balanced=true;
+        height(root,balanced);
+        return balanced;
+    }
+    
 };
