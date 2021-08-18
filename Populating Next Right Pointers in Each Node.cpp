195debcb2d4a83cb9dbdc26721d7885f7bbadc79
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-08-18 22:59:16 +0530

    Update Populating Next Right Pointers in Each Node.cpp

--- Populating Next Right Pointers in Each Node.cpp
+++ Populating Next Right Pointers in Each Node.cpp
@@ -1,3 +1,4 @@
+//Method 1 (RECURSIVE)
 class Solution {
 public:
     
@@ -18,5 +19,43 @@ public:
             return nullptr;
         dfs(root);
         return root;
+    }
+};
+
+//Method 2 (ITERATIVE)
+class Solution {
+public:
+    
+    bool isLeaf(Node* root)
+    {
+        return root->left==nullptr && root->right==nullptr;
+    }
+    
+    Node* connectNodes(Node* root)
+    {
+        root->left->next=root->right;
+        root->right->next=nullptr;
+        Node* cur=root->left;
+        Node* start=root->left;
+        while(cur)
+        {
+            while(cur)
+            {
+                if(cur->left)
+                    cur->left->next=cur->right;
+                if(cur->right && cur->next)
+                    cur->right->next=cur->next->left;
+                cur=cur->next;
+            }
+            start=start->left;
+            cur=start;
+        }
+        return root;
+    }
+    
+    Node* connect(Node* root) {
+        if(root==nullptr || isLeaf(root))
+            return root;
+        return connectNodes(root);
     }
 };
