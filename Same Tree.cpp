d596f2cad90877fb4f65e6705efba1577700fa9a
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-08-25 11:42:18 +0530

    Update Same Tree.cpp

--- Same Tree.cpp
+++ Same Tree.cpp
@@ -1,12 +1,38 @@
+//Method 1(DFS)
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p==nullptr && q==nullptr)
+        if(!p && !q)
             return true;
-        if(p==nullptr && q!=nullptr)
+        if(!p || !q)
             return false;
-        if(p!=nullptr && q==nullptr)
-            return false;
-        return isSameTree(p->left,q->left)==true && isSameTree(p->right,q->right)==true && p->val==q->val;
+        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right) && p->val==q->val;
+    }
+};
+
+//Method 2(BFS)
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        queue<pair<TreeNode*,TreeNode*>> qu;
+        qu.push({p,q});
+        while(!qu.empty())
+        {
+            int n=qu.size();
+            for(int i=0;i<n;i++)
+            {
+                pair<TreeNode*,TreeNode*> curNodes=qu.front();
+                qu.pop();
+                if(!curNodes.first && !curNodes.second)
+                    continue;
+                else if(!curNodes.first || !curNodes.second)
+                    return false;
+                else if(curNodes.first->val!=curNodes.second->val)
+                        return false;
+                qu.push({curNodes.first->left,curNodes.second->left});
+                qu.push({curNodes.first->right,curNodes.second->right});
+            }
+        }
+        return true;
     }
 };
