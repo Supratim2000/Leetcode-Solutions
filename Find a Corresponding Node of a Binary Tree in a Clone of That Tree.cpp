98db1cc0da2adea759e407f1898c7677a483fa8c
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-08-27 12:24:41 +0530

    Update Find a Corresponding Node of a Binary Tree in a Clone of That Tree.cpp

--- Find a Corresponding Node of a Binary Tree in a Clone of That Tree.cpp
+++ Find a Corresponding Node of a Binary Tree in a Clone of That Tree.cpp
@@ -72,3 +72,29 @@ public:
         return nullptr;
     }
 };
+
+//Finding path in original tree using DFS and then going to the node in cloned tree
+class Solution {
+public:
+    void dfs(TreeNode* original,TreeNode* target,string path,string& targetPath)
+    {
+        if(!original)
+            return;
+        if(target==original)
+        {
+            targetPath=path;
+            return;
+        }
+        dfs(original->left,target,path+"l",targetPath);
+        dfs(original->right,target,path+"r",targetPath);
+    }
+    
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+        string targetPath="";
+        dfs(original,target,"",targetPath);
+        TreeNode* ref=cloned;
+        for(int i=0;i<targetPath.size();i++)
+            targetPath[i]=='l'?ref=ref->left:ref=ref->right;
+        return ref;
+    }
+};
