2d5245196dd0fafc0b6cf60b2c6accb1c37f7bbc
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-08-27 12:35:18 +0530

    Update Binary Search Tree to Greater Sum Tree.cpp

--- Binary Search Tree to Greater Sum Tree.cpp
+++ Binary Search Tree to Greater Sum Tree.cpp
@@ -1,29 +1,18 @@
 class Solution {
 public:
-    void rev_inorder(TreeNode* root,int& prev_val,bool& f_time)
+    void reverseInorder(TreeNode* root,int& prev)
     {
-        if(root==nullptr)
-            return ;
-        rev_inorder(root->right,prev_val,f_time);
-        if(f_time==true)
-        {
-            f_time=false;
-            prev_val=root->val;
-        }
-        else
-        {
-            root->val=prev_val+root->val;
-            prev_val=root->val;
-        }
-        rev_inorder(root->left,prev_val,f_time);
+        if(!root)
+            return;
+        reverseInorder(root->right,prev);
+        root->val+=prev;
+        prev=root->val;
+        reverseInorder(root->left,prev);
     }
     
     TreeNode* bstToGst(TreeNode* root) {
-        if(root==nullptr)
-            return nullptr;
-        int prev_val=-1;
-        bool f_time=true;
-        rev_inorder(root,prev_val,f_time);
+        int prev=0;
+        reverseInorder(root,prev);
         return root;
     }
 };
