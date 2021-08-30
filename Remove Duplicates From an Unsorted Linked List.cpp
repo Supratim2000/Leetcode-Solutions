efa5b9cdc85bfe0f4d4f958d99d1fd6f029d11a8
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-08-30 21:24:47 +0530

    Update Remove Duplicates From an Unsorted Linked List.cpp

--- Remove Duplicates From an Unsorted Linked List.cpp
+++ Remove Duplicates From an Unsorted Linked List.cpp
@@ -1,3 +1,4 @@
+//Without deleting duplicates from memory
 class Solution
 {
     public:
@@ -20,6 +21,40 @@ class Solution
                 prev=cur;
             }
             cur=cur->next;
+        }
+        prev->next=nullptr;
+        return head;
+    }
+};
+
+//With deleting duplicates from memory
+class Solution
+{
+    public:
+    //Function to remove duplicates from unsorted linked list.
+    Node* removeDuplicates(Node *head) 
+    {
+        if(!head)
+            return nullptr;
+        unordered_set<int> pres;
+        Node* cur=head;
+        Node* prev=new Node(INT_MIN);
+        prev->next=cur;
+        pres.insert(INT_MIN);
+        while(cur)
+        {
+            if(pres.find(cur->data)==pres.end())
+            {
+                pres.insert(cur->data);
+                prev=cur;
+                cur=cur->next;
+            }
+            else
+            {
+                prev->next=cur->next;
+                delete cur;
+                cur=prev->next;
+            }
         }
         prev->next=nullptr;
         return head;
