588835550a21d9ced232414d27ce3ac3e3d26d43
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-08-31 21:53:13 +0530

    Update Partition List.cpp

--- Partition List.cpp
+++ Partition List.cpp
@@ -1,6 +1,44 @@
+//Method 1
 class Solution {
 public:
-    ListNode* partition(ListNode* head, int x) {
+    ListNode* oddEvenList(ListNode* head) {
+        if(!head || !head->next || !head->next->next)
+            return head;
+        ListNode* p=head;
+        ListNode* q=head->next;
+        ListNode* t=q;
+        ListNode* x=head;
+        ListNode* y=head->next;
+        while(true)
+        {
+            x=x->next->next;
+            p->next=x;
+            p=x;
+            
+            y=y->next->next;
+            q->next=y;
+            q=y;
+            
+            if(!q && !p->next)
+            {
+                p->next=t;
+                break;
+            }
+            if(!q->next && !p->next->next) {
+                p->next=t;
+                break;
+            }
+        }
+        return head;
+    }
+};
+
+
+//Method 2(Using two pointer)
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        int count=1;
         ListNode* newHead;
         ListNode* d1=new ListNode(INT_MAX);
         ListNode* d2=new ListNode(INT_MAX);
@@ -9,31 +47,23 @@ public:
         ListNode* p=head;
         while(p)
         {
-            if(p->val<x)
+            if(count&1)
             {
-                if(l1->next!=p)
-                    l1->next=p;
+                l1->next=p;
                 l1=p;
                 l2->next=nullptr;
             }
             else
             {
-                if(l2->next!=p)
-                    l2->next=p;
+                l2->next=p;
                 l2=p;
                 l1->next=nullptr;
             }
+            count++;
             p=p->next;
         }
-        if(!d1->next)
-            newHead=d2->next;
-        else if(!d2->next)
-            newHead=d1->next;
-        else
-        {
-            newHead=d1->next;
-            l1->next=d2->next;
-        }
+        newHead=d1->next;
+        l1->next=d2->next;
         delete d1;
         delete d2;
         return newHead;
