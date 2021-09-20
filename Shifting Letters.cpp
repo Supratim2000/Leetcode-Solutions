a91d3eb6b5c64b8514972b25363f6fc3563cb5c1
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-09-20 23:08:36 +0530

    Create Shifting Letters.cpp

---
+++ Shifting Letters.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
+        int n=shifts.size();
+        for(int i=n-2;i>=0;i--)
+            shifts[i]=(shifts[i]+shifts[i+1])%26;
+        for(int i=0;i<n;i++)
+            s[i]=(((s[i]-97)+shifts[i])%26)+97;
+        return s;
+    }
+};
