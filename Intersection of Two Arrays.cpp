3ecc779eb1fb59401032d0b8326971735fe37d67
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-09-20 20:49:21 +0530

    Update Intersection of Two Arrays.cpp

--- Intersection of Two Arrays.cpp
+++ Intersection of Two Arrays.cpp
@@ -1,3 +1,4 @@
+//Approach 1(Using hashing)
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
@@ -11,6 +12,43 @@ public:
         for(int i=0;i<m;i++)
         {
             if(pres.find(nums2[i])!=pres.end() && doneInNums2.find(nums2[i])==doneInNums2.end())
+            {
+                ans.push_back(nums2[i]);
+                doneInNums2.insert(nums2[i]);
+            }
+        }
+        return ans;
+    }
+};
+
+//Approach 2(Using Sorting and binary search)
+class Solution {
+public:
+    bool binSrc(vector<int>& arr,int key,int low,int high)
+    {
+        while(low<=high)
+        {
+            int mid=low+((high-low)/2);
+            if(arr[mid]==key)
+                return true;
+            else if(key<arr[mid])
+                high=mid-1;
+            else
+                low=mid+1;
+        }
+        return false;
+    }
+    
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums1.size();
+        int m=nums2.size();
+        vector<int> ans;
+        unordered_set<int> doneInNums2;
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        for(int i=0;i<m;i++)
+        {
+            if(doneInNums2.find(nums2[i])==doneInNums2.end() && binSrc(nums1,nums2[i],0,n-1))
             {
                 ans.push_back(nums2[i]);
                 doneInNums2.insert(nums2[i]);
