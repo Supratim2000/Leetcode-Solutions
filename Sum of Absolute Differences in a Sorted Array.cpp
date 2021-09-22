fda496054503c966a9cded017f66fca8548fb44e
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-09-22 12:56:43 +0530

    Update Sum of Absolute Differences in a Sorted Array.cpp

--- Sum of Absolute Differences in a Sorted Array.cpp
+++ Sum of Absolute Differences in a Sorted Array.cpp
@@ -1,3 +1,5 @@
+//Time Complexity:- O(N)
+//Space Complexity:- O(N)
 class Solution {
 public:
     vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
@@ -15,6 +17,26 @@ public:
         }
         ans[0]=last-((n-1)*nums[0]);
         ans[n-1]=((n-1)*nums[n-1])-pf[n-2];
+        return ans;
+    }
+};
+
+//Time Complexity:- O(N)
+//Space Complexity:- O(1)
+class Solution {
+public:
+    vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans(n,0);
+        int sum=0;
+        int sumTillIndex=0;
+        for(auto x: nums)
+            sum+=x;
+        for(int i=0;i<n;i++)
+        {
+            sumTillIndex+=nums[i];
+            ans[i]=((i+1)*nums[i])-sumTillIndex+(sum-sumTillIndex)-((n-i-1)*nums[i]);
+        }
         return ans;
     }
 };
