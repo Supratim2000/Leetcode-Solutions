50d857c6bf4f90321b8d485d64871cd160da4418
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-09-22 12:29:48 +0530

    Create Sum of Absolute Differences in a Sorted Array.cpp

---
+++ Sum of Absolute Differences in a Sorted Array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
+        int n=nums.size();
+        int last=nums[n-1];
+        vector<int> ans(n,0);
+        vector<int> pf(n,0);
+        pf[0]=nums[0];
+        for(int i=1;i<n;i++)
+            pf[i]=nums[i]+pf[i-1];
+        for(int i=n-2;i>=1;i--)
+        {
+            ans[i]=(i*nums[i])-pf[i-1]+last-((n-i-1)*nums[i]);
+            last=last+nums[i];
+        }
+        ans[0]=last-((n-1)*nums[0]);
+        ans[n-1]=((n-1)*nums[n-1])-pf[n-2];
+        return ans;
+    }
+};
