48dcad4ee85af10358ca7282aab7e2a37e75107e
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-09-25 23:32:39 +0530

    Create Find the Smallest Divisor Given a Threshold.cpp

---
+++ Find the Smallest Divisor Given a Threshold.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int maximum(vector<int>& nums,int& n)
+    {
+        int mx=INT_MIN;
+        for(auto x: nums)
+            mx=max(mx,x);
+        return mx;
+    }
+    
+    long long divSum(vector<int>& nums,int& n,int d)
+    {
+        long long add=0;
+        for(auto x: nums)
+            add+=((x+d-1)/d);
+        return add;
+    }
+    
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int ans=INT_MIN;
+        int n=nums.size();
+        int l=1;
+        int h=maximum(nums,n)+1;
+        while(l<=h)
+        {
+            int mid=(l+h)/2;
+            long long res=divSum(nums,n,mid);
+            if(res<=threshold)
+            {
+                ans=mid;
+                h=mid-1;
+            }
+            else
+                l=mid+1;
+        }
+        return ans;
+    }
+};
