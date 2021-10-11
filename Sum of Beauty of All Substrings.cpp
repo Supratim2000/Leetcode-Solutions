c8b061433a87dac7600f3559d1db05eeab05835a
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-10-11 21:28:22 +0530

    Update Sum of Beauty of All Substrings.cpp

--- Sum of Beauty of All Substrings.cpp
+++ Sum of Beauty of All Substrings.cpp
@@ -1,3 +1,4 @@
+//Time complexity:- O(26*n^2)
 class Solution {
 public:
     int beautySum(string s) {
@@ -21,6 +22,32 @@ public:
                         continue;
                 }
                 beauty+=(mx-mn);
+            }
+        }
+        return beauty;
+    }
+};
+
+
+//Slight optimization.
+//Time complexity:- O((n^2)*logn)
+class Solution {
+public:
+    int beautySum(string s) {
+        int beauty=0;
+        int n=s.size();
+        for(int i=0;i<n;i++)
+        {
+            vector<int> freq(26,0);
+            multiset<int> count;
+            for(int j=i;j<n;j++)
+            {
+                auto it=count.find(freq[s[j]-'a']);
+                if(it!=count.end())
+                    count.erase(it);
+                freq[s[j]-'a']++;
+                count.insert(freq[s[j]-'a']);
+                beauty+=(*(count.rbegin())-(*count.begin()));
             }
         }
         return beauty;
