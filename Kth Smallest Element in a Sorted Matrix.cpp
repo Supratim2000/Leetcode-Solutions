e38934498a18bbfc3f8353e610860f981277c05f
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2021-10-14 00:51:07 +0530

    Update Kth Smallest Element in a Sorted Matrix.cpp

--- Kth Smallest Element in a Sorted Matrix.cpp
+++ Kth Smallest Element in a Sorted Matrix.cpp
@@ -1,4 +1,4 @@
-//Using priority queue
+//Using Heap
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
@@ -9,6 +9,48 @@ public:
                 maxHeap.push(element);
         for(int i=0;i<n*n-k;i++)
             maxHeap.pop();
-        return maxHeap.top();;
+        return maxHeap.top();
+    }
+};
+
+//Using Binary search
+class Solution {
+public:
+    int smallerThan(vector<vector<int>>& matrix,int n,int val)
+    {
+        int count=0;
+        int i=0;
+        int j=n-1;
+        while(i<n && j>=0)
+        {
+            if(matrix[i][j]<=val)
+            {
+                count+=j+1;
+                i++;
+            }
+            else
+                j--;
+        }
+        return count;
+    }
+    
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n=matrix.size();
+        int low=matrix[0][0];
+        int high=matrix[n-1][n-1];
+        if(k==1)
+            return matrix[0][0];
+        if(k==n*n)
+            return matrix[n-1][n-1];
+        while(low<high)
+        {
+            int mid=low+(high-low)/2;
+            int m=smallerThan(matrix,n,mid);
+            if(m<k)
+                low=mid+1;
+            else
+                high=mid;
+        }
+        return low;
     }
 };
