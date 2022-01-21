fcf627dcf3b47e45e15a9ebbe7d25d62e6427ea5
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2022-01-21 11:48:38 +0530

    Update Maximum Depth of Binary Tree.cpp

--- Maximum Depth of Binary Tree.cpp
+++ Maximum Depth of Binary Tree.cpp
@@ -1,6 +1,26 @@
+//Solution - 1
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
         return root==nullptr?0:max(maxDepth(root->left),maxDepth(root->right))+1;
+    }
+};
+
+//Solution - 2
+class Solution {
+public:    
+    void helper(TreeNode* root,int curDep,int& maxDep) {
+        if(!root)
+            return;
+        helper(root->left,curDep+1,maxDep);
+        if(curDep>maxDep)
+            maxDep=curDep;
+        helper(root->right,curDep+1,maxDep);
+    }
+    
+    int maxDepth(TreeNode* root) {
+        int maxDep=0;
+        helper(root, 1, maxDep);
+        return maxDep;
     }
 };
