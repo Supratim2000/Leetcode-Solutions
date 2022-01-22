53f52b9bb373bcaf09c72706f669a9cf339ed9e7
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2022-01-22 15:26:26 +0530

    Update Binary Tree Zigzag Level Order Traversal.cpp

--- Binary Tree Zigzag Level Order Traversal.cpp
+++ Binary Tree Zigzag Level Order Traversal.cpp
@@ -1,3 +1,4 @@
+//Method-1 (By reversing vector)
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
@@ -32,6 +33,40 @@ public:
             ans.push_back(level);
             toggle=!toggle;
             level.clear();
+        }
+        return ans;
+    }
+};
+
+//Method-2 (Without reversing vector)
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(!root)
+            return ans;
+        bool toggle=false;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int n=q.size();
+            vector<int> level(n);
+            for(int i=0;i<n;i++)
+            {
+                TreeNode* curNode=q.front();
+                q.pop();
+                int index=i;
+                if(toggle)
+                    index=n-i-1;
+                level[index]=curNode->val;
+                if(curNode->left)
+                    q.push(curNode->left);
+                if(curNode->right)
+                    q.push(curNode->right);
+            }
+            ans.push_back(level);
+            toggle=!toggle;
         }
         return ans;
     }
