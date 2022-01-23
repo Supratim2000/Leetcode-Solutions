26363be2fccf5d2a2590a00cc71f7fa76c2af1dd
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2022-01-23 20:16:07 +0530

    Update Lowest Common Ancestor of a Binary Tree.cpp

--- Lowest Common Ancestor of a Binary Tree.cpp
+++ Lowest Common Ancestor of a Binary Tree.cpp
@@ -75,29 +75,21 @@ public:
 
 //Optimised code
 class Solution {
-public:
+public:    
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(!root)
-            return root;
-        TreeNode* left=lowestCommonAncestor(root->left,p,q);
-        TreeNode* right=lowestCommonAncestor(root->right,p,q); 
+            return nullptr;
+        TreeNode* left=lowestCommonAncestor(root->left, p, q);
+        TreeNode* right=lowestCommonAncestor(root->right, p, q);
         if(left && right)
             return root;
+        else if(root==p)
+            return p;
+        else if(root==q)
+            return q;
         else if(left || right)
-        {
-            if(root==p)
-                return p;
-            else if(root==q)
-                return q;
             return left?left:right;
-        }
         else
-        {
-            if(root==p)
-                return p;
-            else if(root==q)
-                return q;
             return nullptr;
-        }
     }
 };
