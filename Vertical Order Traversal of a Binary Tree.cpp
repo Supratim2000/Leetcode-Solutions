3edb6b5708dceffc8181c7b7d697b8ea812ca39f
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2022-01-26 18:35:49 +0530

    Update Vertical Order Traversal of a Binary Tree.cpp

--- Vertical Order Traversal of a Binary Tree.cpp
+++ Vertical Order Traversal of a Binary Tree.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-    void dfs(TreeNode* root,int wide,int level,map<int,map<int,multiset<int>>>& mp)
+    void dfs(TreeNode* root,int row ,int col, map<int, map<int, multiset<int>>>& mp)
     {
-        mp[wide][level].insert(root->val);   
-        if(root->left)
-            dfs(root->left,wide-1,level+1,mp);
-        if(root->right)
-            dfs(root->right,wide+1,level+1,mp);
+        if(!root)
+            return;
+        mp[col][row].insert(root->val);
+        dfs(root->left,row+1,col-1,mp);
+        dfs(root->right,row+1,col+1,mp);
     }
     
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int,map<int,multiset<int>>> mp;
         vector<vector<int>> ans;
-        if(root==nullptr)
-            return ans;
-        int level=0;
-        int wide=0;
-        dfs(root,wide,level,mp);
-        
+        map<int, map<int, multiset<int>>> mp;
+        dfs(root,0,0,mp);
         for(auto x: mp)
         {
             vector<int> temp;
-            for(auto i:x.second)
-                for(auto y: i.second)
-                    temp.push_back(y);
+            for(auto y: x.second)
+                for(auto z: y.second)
+                    temp.push_back(z);
             ans.push_back(temp);
         }
         return ans;
