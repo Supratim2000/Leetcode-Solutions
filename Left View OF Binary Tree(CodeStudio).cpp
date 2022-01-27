150de9ebec502b4408bf67d0a7011f4c79341625
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2022-01-27 15:06:41 +0530

    Update Left View OF Binary Tree(CodeStudio).cpp

--- Left View OF Binary Tree(CodeStudio).cpp
+++ Left View OF Binary Tree(CodeStudio).cpp
@@ -1,22 +1,44 @@
-void dfs(BinaryTreeNode<int>* root,int level,int& maxLevel,vector<int>& view)
+//Using DFS
+void dfs(BinaryTreeNode<int>* root,int level,int& maxLevel)
 {
     if(!root)
         return;
     if(level>maxLevel)
     {
         maxLevel=level;
-        view.push_back(root->data);
+        cout<<root->data<<" ";
     }
-    dfs(root->left,level+1,maxLevel,view);
-    dfs(root->right,level+1,maxLevel,view);
+    dfs(root->left,level+1,maxLevel);
+    dfs(root->right,level+1,maxLevel);
 }
 
 void printLeftView(BinaryTreeNode<int> *root)
 {
-	vector<int> view;
-	int maxLevel=-1;
-    dfs(root,0,maxLevel,view);
-    for(auto x: view)
-        cout<<x<<" ";
+    int maxLevel=-1;
+    dfs(root,0,maxLevel);
     cout<<endl;
+}
+
+//Using BFS
+void printLeftView(BinaryTreeNode<int> *root)
+{
+	if(!root)
+        return;
+    queue<BinaryTreeNode<int>*> qu;
+    qu.push(root);
+    while(!qu.empty())
+    {
+        int n=qu.size();
+        for(int i=0;i<n;i++)
+        {
+            BinaryTreeNode<int>* curNode=qu.front();
+            qu.pop();
+            if(i==0)
+                cout<<curNode->data<<" ";
+            if(curNode->left)
+                qu.push(curNode->left);
+            if(curNode->right)
+                qu.push(curNode->right);
+        }
+    }
 }
