e9ee26514966fcb06edad3a3c03151029c57f893
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2022-01-27 12:32:22 +0530

    Update Top View Of The Tree(CodeStudio).cpp

--- Top View Of The Tree(CodeStudio).cpp
+++ Top View Of The Tree(CodeStudio).cpp
@@ -1,3 +1,4 @@
+//Using BFS(Recursive)
 #include<map>
 
 void dfs(TreeNode<int> *root,int spread,int level,map<int,pair<int,int>>& mp)
@@ -21,5 +22,41 @@ vector<int> getTopView(TreeNode<int> *root) {
     dfs(root,0,0,mp);
     for(auto x: mp)
         ans.push_back(x.second.second);
+    return ans;
+}
+
+
+//Using BFS(Iterative)
+#include<map>
+
+void bfs(TreeNode<int> *root,map<int,int>& mp)
+{
+    if(!root)
+        return;
+    queue<pair<TreeNode<int>* ,int>> qu;
+    qu.push({root,0});
+    while(!qu.empty())
+    {
+        int n=qu.size();
+        for(int i=0;i<n;i++)
+        {
+            pair<TreeNode<int>*,int> curNode=qu.front();
+            qu.pop();
+            if(mp.find(curNode.second)==mp.end())
+                mp.insert({curNode.second,curNode.first->val});
+            if(curNode.first->left)
+                qu.push({curNode.first->left,curNode.second-1});
+            if(curNode.first->right)
+                qu.push({curNode.first->right,curNode.second+1});
+        }
+    }
+}
+
+vector<int> getTopView(TreeNode<int> *root) {
+    vector<int> ans;
+    map<int,int> mp; //spread,data
+    bfs(root,mp);
+    for(auto x: mp)
+        ans.push_back(x.second);
     return ans;
 }
