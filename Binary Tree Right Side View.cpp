736649f2f53fb58fc4468db96dc7b33de3f4920f
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2022-01-27 14:50:37 +0530

    Update Binary Tree Right Side View.cpp

--- Binary Tree Right Side View.cpp
+++ Binary Tree Right Side View.cpp
@@ -1,31 +1,53 @@
+//Using DFS
+class Solution {
+public:
+    void traverse(TreeNode *root,int height,int& maxHeightTillNow,vector<int>& view)
+    {
+        if(root==nullptr)
+            return;
+        if(height>maxHeightTillNow)
+        {
+            maxHeightTillNow=height;
+            view.push_back(root->val);
+        }
+        traverse(root->right,height+1,maxHeightTillNow,view);
+        traverse(root->left,height+1,maxHeightTillNow,view);
+    }
+    
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> view;
+        int maxHeightTillNow=-1;
+        if(root==nullptr)
+            return view;
+        traverse(root,0,maxHeightTillNow,view);
+        return view;
+    }
+};
+
+//Using BFS
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        vector<int> ans;
-        if(root==nullptr)
-            return ans;
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty())
+        vector<int> view;
+        if(!root)
+            return view;
+        queue<TreeNode*> qu;
+        qu.push(root);
+        while(!qu.empty())
         {
-            bool flag=true;
-            int n=q.size();
+            int n=qu.size();
             for(int i=0;i<n;i++)
             {
-                TreeNode* curNode=q.front();
-                q.pop();
-                if(flag)
-                {
-                    ans.push_back(curNode->val);
-                    flag=false;
-                }
-                if(curNode->right)
-                    q.push(curNode->right);
+                TreeNode* curNode=qu.front();
+                qu.pop();
+                if(i==n-1)
+                    view.push_back(curNode->val);
                 if(curNode->left)
-                    q.push(curNode->left);
+                    qu.push(curNode->left);
+                if(curNode->right)
+                    qu.push(curNode->right);
             }
-            flag=true;
         }
-        return ans;
+        return view;
     }
 };
