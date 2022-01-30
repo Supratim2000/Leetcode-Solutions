5b9a395b42d742e29cb708bf74b3c1a33fe3159e
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2022-01-30 16:32:31 +0530

    Create Construct Binary Tree from Inorder and Postorder Traversal.cpp

---
+++ Construct Binary Tree from Inorder and Postorder Traversal.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    TreeNode* create(vector<int>& inorder, vector<int>& postorder, unordered_map<int,int>& hash, int is, int ie, int ps, int pe)
+    {
+        if(is>ie || ps>pe)
+            return nullptr;
+        int rootIndxInInorder=hash[postorder[pe]];
+        int lps=ps;
+        int lpe=lps+rootIndxInInorder-is-1;
+        int lis=is;
+        int lie=rootIndxInInorder-1;
+        int rps=lpe+1;
+        int rpe=pe-1;
+        int ris=rootIndxInInorder+1;
+        int rie=ie;
+        TreeNode* root=new TreeNode(postorder[pe]);
+        root->left=create(inorder,postorder,hash,lis,lie,lps,lpe);
+        root->right=create(inorder,postorder,hash,ris,rie,rps,rpe);
+        return root;
+    }
+    
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        int n=inorder.size();
+        unordered_map<int,int> hash;
+        for(int i=0;i<n;i++)
+            hash[inorder[i]]=i;
+        return create(inorder,postorder,hash,0,n-1,0,n-1);
+    }
+};
