29e75b4a03008683a652da5fe95650b5fddfb6f5
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2022-04-02 13:20:22 +0530

    Update All Ancestors of a Node in a Directed Acyclic Graph.cpp

--- All Ancestors of a Node in a Directed Acyclic Graph.cpp
+++ All Ancestors of a Node in a Directed Acyclic Graph.cpp
@@ -1,4 +1,4 @@
-//Approach 1
+//Approach 1(O(n^2))
 class Solution {
 public:
     void dfs(int src,vector<int> adj[],vector<bool>& vis)
@@ -22,6 +22,39 @@ public:
                 if(vis[j] && j!=i)
                     ans[j].push_back(i);
         }
+        return ans;
+    }
+};
+
+
+//Approach 2(O(n^2*logn))
+class Solution {
+public:
+    void dfs(int src,vector<int> adj[],vector<bool>& vis)
+    {
+        vis[src]=true;
+        for(auto x: adj[src])
+            if(!vis[x])
+                dfs(x,adj,vis);
+    }
+    
+    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> ans(n);
+        vector<set<int>> temp(n);
+        vector<int> adj[n];
+        for(auto x: edges)
+            adj[x[1]].push_back(x[0]);
+        for(int i=0;i<n;i++)
+        {
+            vector<bool> vis(n,false);
+            dfs(i,adj,vis);
+            for(int j=0;j<n;j++)
+                if(vis[j] && j!=i)
+                    temp[i].insert(j);
+        }
+        for(int i=0;i<n;i++)
+            for(auto x:temp[i])
+                ans[i].push_back(x);
         return ans;
     }
 };
