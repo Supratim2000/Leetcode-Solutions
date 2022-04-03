50f9cac5203926c74af9e3ca71fff4cf8e84d4a0
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2022-04-03 18:23:01 +0530

    Update Validate Binary Tree Nodes.cpp

--- Validate Binary Tree Nodes.cpp
+++ Validate Binary Tree Nodes.cpp
@@ -111,3 +111,74 @@ public:
         return (numOfComponent>1 || numOne!=n-1)?false:true;
     }
 };
+
+//Approach 3(Using DSU)
+class DSU {
+    public:
+        vector<int> parent;
+        vector<int> rank;
+        DSU(int v)
+        {
+            for(int i=0;i<=v;i++)
+            {
+                parent.push_back(i);
+                rank.push_back(1);
+            }
+        }
+        
+        int find(int v)
+        {
+            if(v==parent[v])
+                return v;
+            int leader=find(parent[v]);
+            parent[v]=leader;
+            return leader;
+        }
+    
+        void Union(int u, int v)
+        {
+            int lu=find(u);
+            int lv=find(v);
+            if(lu!=lv)
+            {
+                if(rank[lu]>rank[lv])
+                    parent[lv]=lu;
+                else if(rank[lu]<rank[lv])
+                    parent[lu]=lv;
+                else
+                {
+                    parent[lv]=lu;
+                    rank[lu]++;
+                }
+            }
+        }    
+};
+class Solution {
+public:
+    bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
+        vector<int> parentCount(n,0);
+        DSU* dsu=new DSU(n);
+        int numOfComponent=0;
+        int numOne=0;
+        for(int i=0;i<n;i++)
+        {
+            if(leftChild[i]!=-1)
+            {
+                dsu->Union(i,leftChild[i]);
+                parentCount[leftChild[i]]++;
+            }
+            if(rightChild[i]!=-1)
+            {
+                dsu->Union(i,rightChild[i]);
+                parentCount[rightChild[i]]++;
+            }
+        }            
+        for(int i=0;i<n;i++)
+            if(parentCount[i]==1)
+                numOne++;
+        for(int i=0;i<n;i++)
+            if(dsu->parent[i]==i)
+                numOfComponent++;
+        return (numOfComponent>1 || numOne!=n-1)?false:true;
+    }
+};
