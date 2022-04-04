d4d21ef132cb7834e7bd8f281d6afa46548c287e
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2022-04-04 17:56:20 +0530

    Update Car Pooling.cpp

--- Car Pooling.cpp
+++ Car Pooling.cpp
@@ -1,3 +1,4 @@
+//Brute-force
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
@@ -10,6 +11,52 @@ public:
                 if(capCurPoint[i]>capacity)
                     return false;
             }
+        }
+        return true;
+    }
+};
+
+//Using Map
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        map<int,int> netPas;
+        for(auto x: trips)
+        {
+            netPas[x[1]]+=x[0];
+            netPas[x[2]]-=x[0];
+        }
+        for(auto x: netPas)
+        {
+            capacity-=x.second;
+            if(capacity<0)
+                return false;
+        }
+        return true;
+    }
+};
+
+//Using Sorting
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        vector<vector<int>> timeline;
+        for(auto x: trips)
+        {
+            vector<int> a(3,0);
+            vector<int> b(3,0);
+            a[0]=x[1]; a[1]=1; a[2]=x[0];
+            b[0]=x[2]; b[1]=0; b[2]=x[0];
+            timeline.push_back(a);
+            timeline.push_back(b);
+        }
+        sort(timeline.begin(),timeline.end());
+        int cur=0;
+        for(auto x: timeline)
+        {
+            x[1]==1?capacity-=x[2]:capacity+=x[2];
+            if(capacity<0)
+                return false;
         }
         return true;
     }
