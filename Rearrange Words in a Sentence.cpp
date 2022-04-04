1037d29cec8d2958b15ebe3be98deadf9cc628e0
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2022-04-04 18:19:59 +0530

    Update Rearrange Words in a Sentence.cpp

--- Rearrange Words in a Sentence.cpp
+++ Rearrange Words in a Sentence.cpp
@@ -1,3 +1,4 @@
+//Using Map
 class Solution {
 public:
     string arrangeWords(string text) {
@@ -29,6 +30,45 @@ public:
                     x[0]=x[0]+32;
                 ans+=x;
                 ans+=" ";
+            }
+        }
+        ans.pop_back();
+        ans[0]=ans[0]-32;
+        return ans;
+    }
+};
+
+//Using Array of vectors
+class Solution {
+public:
+    string arrangeWords(string text) {
+        vector<string> adj[100001];
+        int n=text.size();
+        text[0]=text[0]+32;
+        string word="";
+        string ans="";
+        int curLen=0;
+        for(int i=0;i<n;i++)
+        {
+            if(text[i]==' ')
+            {
+                adj[curLen].push_back(word);
+                word="";
+                curLen=0;
+            }
+            else
+            {
+                word+=text[i];
+                curLen++;
+            }
+        }
+        adj[curLen].push_back(word);
+        for(int i=0;i<100001;i++)
+        {
+            for(auto x: adj[i])
+            {
+                ans+=x;
+                ans+=' ';
             }
         }
         ans.pop_back();
