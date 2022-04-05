02bc6c968f9a9c5037d16e020ff411113a0333d3
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2022-04-05 20:42:51 +0530

    Update Array With Elements Not Equal to Average of Neighbors.cpp

--- Array With Elements Not Equal to Average of Neighbors.cpp
+++ Array With Elements Not Equal to Average of Neighbors.cpp
@@ -1,3 +1,4 @@
+//Method 1
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
@@ -13,6 +14,22 @@ public:
         }
         if(j+2==i)
             ans.pop_back();
+        return ans;
+    }
+};
+
+//Method 2
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        vector<int> ans(n,0);
+        int j=0;
+        for(int i=0;i<n;i+=2)
+            ans[i]=nums[j++];
+        for(int i=1;i<n;i+=2)
+            ans[i]=nums[j++];
         return ans;
     }
 };
