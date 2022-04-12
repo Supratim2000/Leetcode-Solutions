65ed5d440bd7b813c8a29d2d3cbc367fc7435caa
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2022-04-12 21:43:10 +0530

    Update Complete Binary Tree Inserter.cpp

--- Complete Binary Tree Inserter.cpp
+++ Complete Binary Tree Inserter.cpp
@@ -1,3 +1,4 @@
+//Using Heap style concept
 class CBTInserter {
 public:
     vector<TreeNode*> nodeList;
@@ -32,6 +33,55 @@ public:
             TreeNode* curNode=q.front();
             q.pop();
             nodeList.push_back(curNode);
+            if(curNode->left)
+                q.push(curNode->left);
+            if(curNode->right)
+                q.push(curNode->right);
+        }
+    }
+};
+
+
+//Using Queue
+class CBTInserter {
+public:
+    queue<TreeNode*> adjNodes;
+    TreeNode* root=nullptr;
+    CBTInserter(TreeNode* root) {
+        this->root=root;
+        levelOrder(root);
+    }
+    
+    int insert(int val) {
+        TreeNode* insNode=new TreeNode(val);
+        TreeNode* parent=adjNodes.front();
+        if(parent->left==nullptr)
+        {
+            parent->left=insNode;
+            adjNodes.push(insNode);
+        }
+        else if(parent->right==nullptr)
+        {
+            parent->right=insNode;
+            adjNodes.push(insNode);
+            adjNodes.pop();
+        }
+        return parent->val;
+    }
+    
+    TreeNode* get_root() {
+        return this->root;
+    }
+    
+    void levelOrder(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            TreeNode* curNode=q.front();
+            q.pop();
+            if(curNode->left==nullptr || curNode->right==nullptr)
+                adjNodes.push(curNode);
             if(curNode->left)
                 q.push(curNode->left);
             if(curNode->right)
