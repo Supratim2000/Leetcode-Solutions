03b2683f85d17daff0c16a9de23f139f2530fbbd
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2022-04-13 15:12:18 +0530

    Update Permutations.cpp

--- Permutations.cpp
+++ Permutations.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> permute(vector<int>& nums) {
-        if(nums.size()==1)
-        {
-            vector<vector<int>> ans;
+    void perm(vector<int>& nums, int n, int i, vector<vector<int>>& ans)
+    {
+        if(i==n)
             ans.push_back(nums);
-            return ans;
-        }
-        vector<vector<int>> res;
-        for(int i=0;i<nums.size();i++)
+        else
         {
-            int erased=nums[i];
-            vector<int> temp;
-            for(int j=0;j<nums.size();j++)
-                if(i!=j)
-                    temp.push_back(nums[j]);
-            vector<vector<int>> x=permute(temp);
-            for(int k=0;k<x.size();k++)
+            for(int j=i;j<n;j++)
             {
-                x[k].insert(x[k].begin(),erased);
-                res.push_back(x[k]);
-            } 
+                swap(nums[i],nums[j]);
+                perm(nums,n,i+1,ans);
+                swap(nums[i],nums[j]);
+            }   
         }
-        return res;
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        perm(nums,nums.size(),0,ans);
+        return ans;
     }
 };
