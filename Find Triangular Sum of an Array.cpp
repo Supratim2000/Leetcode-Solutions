705b9cd6ab10bca0e7859a0209924ea95f9dc462
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2022-04-26 00:38:19 +0530

    Update Find Triangular Sum of an Array.cpp

--- Find Triangular Sum of an Array.cpp
+++ Find Triangular Sum of an Array.cpp
@@ -1,3 +1,6 @@
+//Iterative
+//Time complexity:- O(n^2)
+//Space complexity:- O(n) (Due to recursion stack)
 class Solution {
 public:
     int rec(vector<int> nums, int n)
@@ -11,5 +14,19 @@ public:
     int triangularSum(vector<int>& nums) {
         int n=nums.size();
         return rec(nums,n);
+    }
+};
+
+//Iterative
+//Time complexity:- O(n^2)
+//Space complexity:- O(1)
+class Solution {
+public:
+    int triangularSum(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n-1;i++)
+            for(int j=0;j<n-i-1;j++)
+                nums[j]=(nums[j]+nums[j+1])%10;
+        return nums.at(0);
     }
 };
