c61f1fdaf074b7f4192c1352de0a652c7fbeb908
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2022-04-25 21:40:05 +0530

    Update Set Matrix Zeroes.cpp

--- Set Matrix Zeroes.cpp
+++ Set Matrix Zeroes.cpp
@@ -35,12 +35,8 @@ public:
             }
         }
         for(int i=1;i<r;i++)
-            if(matrix[i][0]==0)
-                for(int j=1;j<c;j++)
-                    matrix[i][j]=0;
-        for(int j=1;j<c;j++)
-            if(matrix[0][j]==0)
-                for(int i=0;i<r;i++)
+            for(int j=1;j<c;j++)
+                if(matrix[0][j]==0 || matrix[i][0]==0)
                     matrix[i][j]=0;
         if(hzr)
             for(int j=0;j<c;j++)
