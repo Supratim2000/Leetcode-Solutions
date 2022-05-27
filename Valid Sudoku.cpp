f566f728a9490cc656e0692e9bcc09d5e108a6c9
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2022-05-27 21:32:57 +0530

    Update Valid Sudoku.cpp

--- Valid Sudoku.cpp
+++ Valid Sudoku.cpp
@@ -1,3 +1,29 @@
+//Time complexity:- O(n^2)(Single pass), Space Complexity:- O(n^2)
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<unordered_set<char>> rowHash(10);
+        vector<unordered_set<char>> columnHash(10);
+        vector<unordered_set<char>> blockHash(10);
+        
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+                if(board[i][j]=='.')
+                    continue;
+                char val=board[i][j];
+                if((rowHash[i+1].find(val)!=rowHash[i+1].end()) || (columnHash[j+1].find(val)!=columnHash[j+1].end()) || (blockHash[(i/3)*3+j/3+1].find(val)!=blockHash[(i/3)*3+j/3+1].end()))  
+                    return false;
+                rowHash[i+1].insert(board[i][j]);
+                columnHash[j+1].insert(board[i][j]);
+                blockHash[(i/3)*3+j/3+1].insert(board[i][j]);
+            }
+        }
+        return true;
+    }
+};
+
 //Time complexity:- O(n^2)+O(n^2)+O(n^2), Space Complexity:- O(n)
 class Solution {
 public:
