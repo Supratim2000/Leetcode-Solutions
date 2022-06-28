4d3a73fbf5c29c3452ec276c0b419f1b2ab81bc4
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2022-06-28 15:26:52 +0530

    Update Loud and Rich.cpp

--- Loud and Rich.cpp
+++ Loud and Rich.cpp
@@ -1,3 +1,4 @@
+//DFS with memoization
 class Solution {
 public:
     int dfs(int cur,vector<vector<int>>& adj,vector<int>& quiet,vector<int>& ans)
@@ -27,6 +28,48 @@ public:
             adj[x[1]].push_back(x[0]);
         for(int i=0;i<n;i++)
             dfs(i,adj,quiet,ans);
+        return ans;
+    }
+};
+
+//Topological Sort
+class Solution {
+public:
+    void topo(vector<vector<int>>& adj,vector<int>& quiet,vector<int>& indeg,vector<int>& ans)
+    {
+        int n=indeg.size();
+        queue<int> q;
+        for(int i=0;i<n;i++)
+            if(indeg[i]==0)
+                q.push(i);
+        while(!q.empty())
+        {
+            int cur=q.front();
+            q.pop();
+            for(auto x: adj[cur])
+            {
+                if(quiet[ans[cur]]<quiet[ans[x]])
+                    ans[x]=ans[cur];
+                indeg[x]--;
+                if(indeg[x]==0)
+                    q.push(x);
+            }
+        }
+    }
+    
+    vector<int> loudAndRich(vector<vector<int>>& richer, vector<int>& quiet) {
+        int n=quiet.size();
+        vector<int> ans(n);
+        for(int i=0;i<n;i++)
+            ans[i]=i;
+        vector<int> indeg(n,0);
+        vector<vector<int>> adj(n);
+        for(auto x: richer)
+        {
+            adj[x[0]].push_back(x[1]);
+            indeg[x[1]]++;
+        }
+        topo(adj,quiet,indeg,ans);
         return ans;
     }
 };
