af3f1fc7d2ec123d73d6a9ed410a5bc0e6fff3b0
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2022-07-12 22:38:59 +0530

    Update Detonate the Maximum Bombs.cpp

--- Detonate the Maximum Bombs.cpp
+++ Detonate the Maximum Bombs.cpp
@@ -1,3 +1,4 @@
+//Unoptimised
 class Solution {
 public:
     bool isBlast(vector<int>& a,vector<int>& b)
@@ -30,6 +31,55 @@ public:
             int count=0;
             vector<bool> vis(n,false);
             dfs(bombs,vis,n,i,count);
+            maxNum=max(maxNum,count);
+        }
+        return maxNum;
+    }
+};
+
+
+//Optimised
+class Solution {
+public:
+    bool isBlast(vector<int>& a,vector<int>& b)
+    {
+        long long x1=a[0];
+        long long x2=b[0];
+        long long y1=a[1];
+        long long y2=b[1];
+        long long r=a[2];
+        long long x=(x1-x2)*(x1-x2);
+        long long y=(y1-y2)*(y1-y2);
+        return x+y<=r*r;
+    }
+    
+    void dfs(vector<vector<int>>& adj,vector<bool>& vis, int n, int i, int& count)
+    {
+        vis[i]=true;
+        count++;
+        for(auto x: adj[i])
+            if(!vis[x])
+                dfs(adj,vis,n,x,count);
+    }
+    
+    void buildPseudoGraph(vector<vector<int>>& bombs, vector<vector<int>>& adj, int n)
+    {
+        for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++)
+                if(i!=j && isBlast(bombs[i],bombs[j]))
+                    adj[i].push_back(j);
+    }
+    
+    int maximumDetonation(vector<vector<int>>& bombs) {
+        int n=bombs.size();
+        vector<vector<int>> adj(n);
+        int maxNum=0;
+        buildPseudoGraph(bombs,adj,n);
+        for(int i=0;i<n;i++)
+        {
+            int count=0;
+            vector<bool> vis(n,false);
+            dfs(adj,vis,n,i,count);
             maxNum=max(maxNum,count);
         }
         return maxNum;
