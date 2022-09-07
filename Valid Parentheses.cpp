5463609cca4d9ee6da2fdfa01d8a7c4b77bf0fbe
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2022-09-08 00:00:18 +0530

    Create Valid Parentheses.cpp

---
+++ Valid Parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n=s.size();
+        stack<char> st;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+                st.push(s[i]);
+            else
+            {
+                if(st.empty() || st.top()=='(' && s[i]!=')' || st.top()=='{' && s[i]!='}' || st.top()=='[' && s[i]!=']')
+                    return false;
+                st.pop();
+            }
+        }
+        return st.empty()?true:false;
+    }
+};
