77590580ba4f536da1aa6b52a6f42968b7881645
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2023-02-04 15:14:06 +0530

    Create Permutation in String.cpp

---
+++ Permutation in String.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        unordered_map<char,int> mp;
+
+        for(int i=0;i<k;i++)
+            mp[s1[i]]++;
+        int k=s.size();
+        int i=0;
+        int j=0;
+
+    }
+};
