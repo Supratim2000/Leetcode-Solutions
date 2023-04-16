1e3a5661656d8d3681c47bc7be60314367fa4c0a
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2023-04-16 11:55:30 +0530

    Update Subset Sum Equal To K(Codestudio).cpp

--- Subset Sum Equal To K(Codestudio).cpp
+++ Subset Sum Equal To K(Codestudio).cpp
@@ -7,18 +7,18 @@ bool fun(vector<int> &arr, vector<vector<int>> &dp, int n, int i, int target)
         return true;
     if(target<0)
         return false;
-    if(i==n)
-        return target==arr[i];
+    if(i==0)
+        return target==arr[0];
     if(dp[i][target]!=-1)
         return dp[i][target];
-    bool take=fun(arr,dp,n,i+1,target-arr[i]);
-    bool notTake=fun(arr,dp,n,i+1,target);
+    bool take=fun(arr,dp,n,i-1,target-arr[i]);
+    bool notTake=fun(arr,dp,n,i-1,target);
     bool posibility= take || notTake;
     dp[i][target]=posibility?1:0;
     return posibility;
 }
 
 bool subsetSumToK(int n, int k, vector<int> &arr) {
-    vector<vector<int>> dp(n+1,vector<int>(k+1,-1));
-    return fun(arr,dp,n,0,k);
+    vector<vector<int>> dp(n,vector<int>(k+1,-1));
+    return fun(arr,dp,n,n-1,k);
 }
