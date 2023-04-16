ae945df0be5690fa838555fe7237a328b096075f
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2023-04-16 12:32:23 +0530

    Update Subset Sum Equal To K(Codestudio).cpp

--- Subset Sum Equal To K(Codestudio).cpp
+++ Subset Sum Equal To K(Codestudio).cpp
@@ -44,3 +44,29 @@ bool subsetSumToK(int n, int k, vector<int> &arr) {
     }
     return dp[n-1][k];    
 }
+
+//Space optimization
+#include <bits/stdc++.h> 
+
+bool subsetSumToK(int n, int k, vector<int> &arr) {
+    vector<bool> prev(k+1,false);
+    vector<bool> cur(k+1,false);
+    prev[0]=true;
+    if(arr[0]<=k)
+        prev[arr[0]]=true;
+    for(int i=1;i<n;i++)
+    {
+        cur[0]=true;
+        for(int j=1;j<=k;j++)
+        {
+            bool notTake=prev[j];
+            bool take=false;
+            if(j-arr[i]>=0)           
+                take=prev[j-arr[i]];
+            cur[j]= take || notTake;
+        }
+        for(int x=0;x<=k;x++)
+            prev[x]=cur[x];
+    }
+    return prev[k];    
+}
