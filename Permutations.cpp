01c247ea223a2e5833594229e83bcc8af0efd7c2
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2023-04-18 13:24:39 +0530

    Update Permutations.cpp

--- Permutations.cpp
+++ Permutations.cpp
@@ -1,3 +1,36 @@
+//Using extra space
+class Solution {
+public:
+    void permutation(vector<int>& arr, vector<vector<int>>& ans, unordered_set<int> vis, vector<int> ds, int n) {
+        if(ds.size()==n)
+        {
+            ans.push_back(ds);
+            return;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(vis.find(i)==vis.end())
+            {
+                ds.push_back(arr[i]);
+                vis.insert(i);
+                permutation(arr,ans,vis,ds,n);
+                ds.pop_back();
+                vis.erase(i);
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        unordered_set<int> vis;
+        vector<int> ds;
+        int n=nums.size();
+        permutation(nums,ans,vis,ds,n);
+        return ans;
+    }
+};
+
+
+//Without using extra space
 class Solution {
 public:
     void perm(vector<int>& nums, int n, int i, vector<vector<int>>& ans)
