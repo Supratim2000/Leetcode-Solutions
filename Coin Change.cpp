d3688a6aa938a0f0dcfddb2418fbd1a5c2e0f921
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2023-08-15 22:30:36 +0530

    Update Coin Change.cpp

--- Coin Change.cpp
+++ Coin Change.cpp
@@ -1,3 +1,4 @@
+//Memoization
 class Solution {
 public:
     int minimumCoins(int amount, vector<int>& coins, vector<int>& dp) {
@@ -17,5 +18,24 @@ public:
         vector<int> dp(amount+1,-1);
         int answer=minimumCoins(amount, coins, dp);
         return answer==10001?-1:answer;
+    }
+};
+
+//Tabulation
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount+1,-1);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++) {
+            int ans=10001;
+            for(int j=0;j<coins.size();j++) {
+                if(i-coins[j]>=0) {
+                    ans=min(ans,dp[i-coins[j]]+1);
+                }
+            }
+            dp[i]=ans;
+        }
+        return dp[amount]==10001?-1:dp[amount];
     }
 };
