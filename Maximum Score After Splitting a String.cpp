26c4e7c6406a4c65b327ce6e76171beef0c78353
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2025-01-01 12:03:08 +0530

    Update Maximum Score After Splitting a String.cpp

--- Maximum Score After Splitting a String.cpp
+++ Maximum Score After Splitting a String.cpp
@@ -2,23 +2,16 @@ class Solution {
 public:
     int maxScore(string s) {
         int n=s.size();
-        vector<int> count(2,0);
-        for(int i=0;i<n;i++) {
-            if(s[i]=='0') {
-                count[0]++;
-            } else {
-                count[1]++;
-            }
-        }
         int maxScore=0;
         int numZ=0;
-        int numO=count[1];
-        for(int i=0;i<n-1;i++) {
-            if(s[i]=='0') {
-                numZ++;
-            } else {
-                numO--;
+        int numO=0;
+        for(int i=0;i<n;i++) {
+            if(s[i]=='1') {
+                numO++;
             }
+        }
+        for(int i=0;i<n-1;i++) {
+            s[i]=='0'?numZ++:numO--;
             maxScore=max(numZ+numO, maxScore);
         }
         return maxScore;
